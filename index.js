fac7718952ce917d7d75ab031c6a75d01a764ebf
ToPPPi <108341898+ToPPPi@users.noreply.github.com>
2023-12-06 22:55:38 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+(function alertUser () {
+    const button = document.getElementById("sign-up-button");
+    button.addEventListener("click", function() {
+        alert ("Hey, you clicked the button!")
+    });
+}) ();
